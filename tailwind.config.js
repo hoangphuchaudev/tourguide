2ae74d86760747469837a886cb452552732d9f5f
tourguide <hoangphuchaudev>
2024-12-05 14:11:23 +0700

    add nodejs, configtailwind, npm, custom font, primary color, banner

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily:{
+        inter: ['"Inter"','sans-serif'],
+        roboto: ['"Roboto"','sans-serif'],
+      },
+      colors:{
+        primary: '#169CFF'
+      },
+      borderRadius:{
+        btn: '12px'
+      },
+    },
+  },
+  plugins: [],
+}
+
